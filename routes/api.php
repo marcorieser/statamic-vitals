26a0b86e373fde1009079fccd8186000c40146c1
Marco Rieser <marco@rieser.dev>
2023-01-30 15:24:44 +0100

    remove legacy get route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 use MarcoRieser\Vitals\Http\Controllers\VitalsController;
 
 Route::prefix('api/statamic-vitals')->group(function () {
-    // TODO[mr]: remove get route on release (07.04.22 mr)
-    Route::get('vitals', [VitalsController::class, '__invoke']);
     Route::post('vitals', [VitalsController::class, '__invoke']);
 });
